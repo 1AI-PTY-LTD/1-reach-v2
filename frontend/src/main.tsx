afb3c80331f39d4482887119cc5d181270b7d72a
Brij Daniel <brij.daniel@gmail.com>
2026-03-17 18:42:32 +1030

    fix stale data reload

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -16,7 +16,7 @@ if (!PUBLISHABLE_KEY) {
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60 * 5,
+      staleTime: 0,
       retry: 1,
     },
   },
